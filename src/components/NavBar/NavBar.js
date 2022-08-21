4be5056fc337fda5a7f2723eb1992fac833c4538
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-21 19:21:33 -0400

    header navbar font class name change

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,23 +1,42 @@
 import "../NavBar/NavBar.js";
 import React from "react";
 import "./NavBar.css";
-
 import { Routes, Route, Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
     <div>
       <span>
-        <Link to="/">About Me</Link>
+        <Link to="/" className="navBarFont" style={{ textDecoration: "none" }}>
+          About Me
+        </Link>
       </span>
       <span>
-        <Link to="/portfolio">Portfolio</Link>
+        <Link
+          to="/portfolio"
+          className="navBarFont"
+          style={{ textDecoration: "none" }}
+        >
+          Portfolio
+        </Link>
       </span>
       <span>
-        <Link to="/contact">Contact</Link>
+        <Link
+          to="/contact"
+          className="navBarFont"
+          style={{ textDecoration: "none" }}
+        >
+          Contact
+        </Link>
       </span>
       <span>
-        <Link to="/resume">Resume</Link>
+        <Link
+          to="/resume"
+          className="navBarFont"
+          style={{ textDecoration: "none" }}
+        >
+          Resume
+        </Link>
       </span>
     </div>
   );
