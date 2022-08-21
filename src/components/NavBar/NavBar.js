adee19f09dd51f839e39c8725ac1041dc9f19ec7
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-21 18:28:24 -0400

    take away hoevrable on navbarjs

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,18 +7,10 @@ export default function NavBar() {
       <nav class="navbar navbar-inverse">
         <div class="container-fluid">
           <ul class="nav navbar-nav">
-            <Link to="/" id="len1" class="hoverable">
-              About Me
-            </Link>
-            <Link to="/portfolio" id="len1" class="hoverable">
-              Portfolio
-            </Link>
-            <Link to="/contact" id="len1" class="hoverable">
-              Contact
-            </Link>
-            <Link to="/resume" id="len1" class="hoverable">
-              Resume
-            </Link>
+            <Link to="/">About Me</Link>
+            <Link to="/portfolio">Portfolio</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/resume">Resume</Link>
           </ul>
         </div>
       </nav>
