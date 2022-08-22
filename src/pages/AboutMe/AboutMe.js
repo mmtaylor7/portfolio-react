8ed360ed794e99b003b17cdd746610bbbca37734
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-21 22:34:11 -0400

    background making it at the top level

--- src/pages/AboutMe/AboutMe.js
+++ src/pages/AboutMe/AboutMe.js
@@ -1,10 +1,9 @@
 import { Routes, Route, Link } from "react-router-dom";
 import "../Bg.css";
-import "./AboutMe.css";
 
 function AboutMe() {
   return (
-    <div className="container body-bg body-size">
+    <div className="container body-size">
       <span>This is my about me page</span>
       {/* <nav>
         <Link to="/about">About</Link>
