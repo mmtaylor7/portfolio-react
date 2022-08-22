f0301d2901eb07299eda06f12d8f011aff8d50e0
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-21 20:46:52 -0400

    background styling

--- src/pages/AboutMe/AboutMe.js
+++ src/pages/AboutMe/AboutMe.js
@@ -1,9 +1,11 @@
 import { Routes, Route, Link } from "react-router-dom";
+import "../Bg.css";
 import "./AboutMe.css";
 
 function AboutMe() {
   return (
-    <div>
+    <div className="container body-bg body-size">
+      <span>This is my about me page</span>
       {/* <nav>
         <Link to="/about">About</Link>
       </nav> */}
