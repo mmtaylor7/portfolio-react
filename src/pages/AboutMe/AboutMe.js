9496b6a62b9345c1e54a6384e2b151b217d8b0f8
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-22 21:39:13 -0400

    added headshot and about me title

--- src/pages/AboutMe/AboutMe.js
+++ src/pages/AboutMe/AboutMe.js
@@ -1,21 +1,17 @@
 import { Routes, Route, Link } from "react-router-dom";
+import cuteMM from "../assets/headshotpicture.jpg";
 
 import "./AboutMe.css";
 
 function AboutMe() {
   return (
-    <div className="container ">
+    <div className="container body-div">
+      {/* <img alt="Headshot" src="../assets/headshotpicture.jpeg" /> */}
+      <img alt="Headshot" src={require("../assets/Headshot.png")} />
       <div>
+        <div className="page-title">About Me</div>
         <div></div>
-        <div>
-          <div>
-            <div></div>
-          </div>
-        </div>
       </div>
-      {/* <nav>
-        <Link to="/about">About</Link>
-      </nav> */}
     </div>
   );
 }
