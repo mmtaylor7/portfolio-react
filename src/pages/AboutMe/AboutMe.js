44de1838b690503f4913a780135f586f579a29ac
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-23 09:04:47 -0400

    tried to get responsiveness on about me; stuck

--- src/pages/AboutMe/AboutMe.js
+++ src/pages/AboutMe/AboutMe.js
@@ -8,27 +8,33 @@ function AboutMe() {
   return (
     <div className="container body-div">
       <Row>
-        <Col sm={12} md={3}>
+        <Col sm={12} lg={6}>
           <img
             className="img headshot"
             alt="Headshot"
             src={require("../assets/Headshot.png")}
           />
         </Col>
-        <Col sm={12} md={3}>
-          <div className="page-title sm-2">
-            <div className="page-title sm-2">About Me</div>
-            <div></div>
-          </div>
-        </Col>
-        <Col sm={12} md={6}>
-          <div className="about-text sm-2">
-            <div className="about-text sm-2">About Me</div>
-            <div></div>
-          </div>
+        <Col sm={12} lg={6}>
+          <Row sm={12} lg={6}>
+            <div className="page-title">
+              <div className="page-title">About Me</div>
+              <div></div>
+            </div>
+          </Row>
+          <Row sm={12} md={6}>
+            <div className="about-text sm-2">
+              <div className="about-text sm-2">
+                Lorem Ipsum asdkl;fjh askldjfh asdkjhefu djhlasd cat dog fish
+                elephant tiger mouse rat squirrel dog daisy tulip hydrangea bla
+                blah blah sarah sam mary margaret jim sally bryan mary doug
+                jimmy jack margot sydney michael campbell.
+              </div>
+              <div></div>
+            </div>
+          </Row>
         </Col>
       </Row>
-      {/* <img alt="Headshot" src="../assets/headshotpicture.jpeg" /> */}
     </div>
   );
 }
