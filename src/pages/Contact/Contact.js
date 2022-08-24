7f6433d8fbe04bb58779f118640cbd566932033e
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-23 20:04:23 -0400

    copied basic structure into contact pg. from about

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,13 +1,41 @@
 import { Routes, Route, Link } from "react-router-dom";
 import "./Contact.css";
+import { Button, Col, Row } from "react-bootstrap";
 
 function Contact() {
   return (
-    <>
-      {/* <nav>
-        <Link to="/about">About</Link>
-      </nav> */}
-    </>
+    return (
+      <div className="container body-div">
+        <Row>
+          <Col sm={12} lg={6}>
+            <img
+              className="img headshot"
+              alt="Headshot"
+              src={require("../assets/Headshot.png")}
+            />
+          </Col>
+          <Col sm={12} lg={6}>
+            <Row sm={12}>
+              <div className="page-title">
+                <div className="page-title">About Me</div>
+                <div></div>
+              </div>
+            </Row>
+            <Row sm={12}>
+              <div className="about-text">
+                <div className="about-text">
+                  Lorem Ipsum asdkl;fjh askldjfh asdkjhefu djhlasd cat dog fish
+                  elephant tiger mouse rat squirrel dog daisy tulip hydrangea bla
+                  blah blah sarah sam mary margaret jim sally bryan mary doug
+                  jimmy jack margot sydney michael campbell.
+                </div>
+                <div></div>
+              </div>
+            </Row>
+          </Col>
+        </Row>
+      </div>
+    );
   );
 }
 
