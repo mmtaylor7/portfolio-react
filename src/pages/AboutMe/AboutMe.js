90d6bdad735c6f2660a54730d343bde1ca2911c9
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-23 20:01:09 -0400

    fixed columns on about me

--- src/pages/AboutMe/AboutMe.js
+++ src/pages/AboutMe/AboutMe.js
@@ -16,15 +16,15 @@ function AboutMe() {
           />
         </Col>
         <Col sm={12} lg={6}>
-          <Row sm={12} lg={6}>
+          <Row sm={12}>
             <div className="page-title">
               <div className="page-title">About Me</div>
               <div></div>
             </div>
           </Row>
-          <Row sm={12} md={6}>
-            <div className="about-text sm-2">
-              <div className="about-text sm-2">
+          <Row sm={12}>
+            <div className="about-text">
+              <div className="about-text">
                 Lorem Ipsum asdkl;fjh askldjfh asdkjhefu djhlasd cat dog fish
                 elephant tiger mouse rat squirrel dog daisy tulip hydrangea bla
                 blah blah sarah sam mary margaret jim sally bryan mary doug
