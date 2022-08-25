96e26fc401d1426f655f7ae8ee69b1df4259b0e6
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-25 13:09:45 -0400

    import button react/ contact styling

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -3,22 +3,24 @@ import "./Contact.css";
 import React, { Component } from "react";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 class SimpleForm extends Component {
   render() {
     return (
       <Container>
+        <div className="contact-me">Contact Me</div>
         <Form>
           <Form.Group controlId="form.Name">
-            <Form.Label>Name</Form.Label>
+            <Form.Label className="form-titles">Name</Form.Label>
             <Form.Control type="text" placeholder="Enter name" />
           </Form.Group>
           <Form.Group controlId="form.Email">
-            <Form.Label>Email address</Form.Label>
+            <Form.Label className="form-titles">Email address</Form.Label>
             <Form.Control type="email" placeholder="name@example.com" />
           </Form.Group>
           <Form.Group controlId="form.Textarea">
-            <Form.Label>Message</Form.Label>
+            <Form.Label className="form-titles">Message</Form.Label>
             <Form.Control as="textarea" rows={3} />
           </Form.Group>
         </Form>
