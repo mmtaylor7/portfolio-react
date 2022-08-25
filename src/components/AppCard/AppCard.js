56893f943f70a04f6fee10ba7426671aa69316c7
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-25 19:30:41 -0400

    created card component for portfolio pg.

---
+++ src/components/AppCard/AppCard.js
@@ -0,0 +1,19 @@
+import "./AppCard.css";
+import React from "react";
+import "../Portfolio/Portfolio.css";
+import { Button, Col, Row } from "react-bootstrap";
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardImage,
+  MDBBtn,
+  MDBRipple,
+} from "mdb-react-ui-kit";
+
+export default function AppCard() {
+    return (
+      
+    );
+  }
--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -1,46 +1,13 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import "../Portfolio/Portfolio.css";
-import { Button, Col, Row } from "react-bootstrap";
-import {
-  MDBCard,
-  MDBCardBody,
-  MDBCardTitle,
-  MDBCardText,
-  MDBCardImage,
-  MDBBtn,
-  MDBRipple,
-} from "mdb-react-ui-kit";
+import AppCard from "../../components/AppCard/AppCard";
 
 function Portfolio() {
   return (
-    <MDBCard>
-      <MDBRipple
-        rippleColor="light"
-        rippleTag="div"
-        className="bg-image hover-overlay"
-      >
-        <MDBCardImage
-          src="https://mdbootstrap.com/img/new/standard/nature/111.webp"
-          fluid
-          alt="..."
-        />
-        <a>
-          <div
-            className="mask"
-            style={{ backgroundColor: "rgba(251, 251, 251, 0.15)" }}
-          ></div>
-        </a>
-      </MDBRipple>
-      <MDBCardBody>
-        <MDBCardTitle>Card title</MDBCardTitle>
-        <MDBCardText>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </MDBCardText>
-        <MDBBtn href="#">Button</MDBBtn>
-      </MDBCardBody>
-    </MDBCard>
+    <div className="portfolio-card-container">
+      <AppCard></AppCard>
+    </div>
   );
 }
 
