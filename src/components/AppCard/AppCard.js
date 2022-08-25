0d4236e2829d6e2b15a314965a6796be5ac93b93
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-25 19:43:40 -0400

    added row/col to portfolio instead of appcard

--- src/components/AppCard/AppCard.js
+++ src/components/AppCard/AppCard.js
@@ -1,7 +1,5 @@
 import "./AppCard.css";
 import React from "react";
-import "../Portfolio/Portfolio.css";
-import { Button, Col, Row } from "react-bootstrap";
 import {
   MDBCard,
   MDBCardBody,
@@ -14,65 +12,32 @@ import {
 
 export default function AppCard() {
   return (
-    <Row>
-      <Col>
-        <MDBCard>
-          <MDBRipple
-            rippleColor="light"
-            rippleTag="div"
-            className="bg-image hover-overlay"
-          >
-            <MDBCardImage
-              src="https://mdbootstrap.com/img/new/standard/nature/111.webp"
-              fluid
-              alt="..."
-            />
-            <a>
-              <div
-                className="mask"
-                style={{ backgroundColor: "rgba(251, 251, 251, 0.15)" }}
-              ></div>
-            </a>
-          </MDBRipple>
-          <MDBCardBody>
-            <MDBCardTitle>Card title</MDBCardTitle>
-            <MDBCardText>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </MDBCardText>
-            <MDBBtn href="#">Button</MDBBtn>
-          </MDBCardBody>
-        </MDBCard>
-      </Col>
-      <Col>
-        <MDBCard>
-          <MDBRipple
-            rippleColor="light"
-            rippleTag="div"
-            className="bg-image hover-overlay"
-          >
-            <MDBCardImage
-              src="https://mdbootstrap.com/img/new/standard/nature/111.webp"
-              fluid
-              alt="..."
-            />
-            <a>
-              <div
-                className="mask"
-                style={{ backgroundColor: "rgba(251, 251, 251, 0.15)" }}
-              ></div>
-            </a>
-          </MDBRipple>
-          <MDBCardBody>
-            <MDBCardTitle>Card title</MDBCardTitle>
-            <MDBCardText>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </MDBCardText>
-            <MDBBtn href="#">Button</MDBBtn>
-          </MDBCardBody>
-        </MDBCard>
-      </Col>
-    </Row>
+    <MDBCard className="app-card">
+      <MDBRipple
+        rippleColor="light"
+        rippleTag="div"
+        className="bg-image hover-overlay"
+      >
+        <MDBCardImage
+          src="https://mdbootstrap.com/img/new/standard/nature/111.webp"
+          fluid
+          alt="..."
+        />
+        <a>
+          <div
+            className="mask"
+            style={{ backgroundColor: "rgba(251, 251, 251, 0.15)" }}
+          ></div>
+        </a>
+      </MDBRipple>
+      <MDBCardBody>
+        <MDBCardTitle>Card title</MDBCardTitle>
+        <MDBCardText>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </MDBCardText>
+        <MDBBtn href="#">Button</MDBBtn>
+      </MDBCardBody>
+    </MDBCard>
   );
 }
--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -2,11 +2,27 @@ import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import "../Portfolio/Portfolio.css";
 import AppCard from "../../components/AppCard/AppCard";
+import { Row, Col } from "react-bootstrap";
 
 function Portfolio() {
   return (
     <div className="portfolio-card-container">
-      <AppCard></AppCard>
+      <Row>
+        <Col>
+          <AppCard></AppCard>
+        </Col>
+        <Col>
+          <AppCard></AppCard>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <AppCard></AppCard>
+        </Col>
+        <Col>
+          <AppCard></AppCard>
+        </Col>
+      </Row>
     </div>
   );
 }
