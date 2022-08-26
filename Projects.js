a6b97c1b3fb0386cbaa9c595017948abcc1c948c
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-25 21:01:52 -0400

    fixed projects array

--- Projects.js
+++ Projects.js
@@ -1,33 +1,29 @@
-
-
-const Projects [
-
-{
+const projects = [
+  {
     id: 1,
     image: lkjhlkjhkl,
     projectTitle: "Doomsday Tracker",
-    description: "An end-of-the-world themed tracker that follows natural disaster data, COVID-19 data and global warming data",
-    github:
-      "https://github.com/mmtaylor7/Doomsday_Tracker",
-    website: "https://mmtaylor7.github.io/Doomsday_Tracker/"
+    description:
+      "An end-of-the-world themed tracker that follows natural disaster data, COVID-19 data and global warming data",
+    github: "https://github.com/mmtaylor7/Doomsday_Tracker",
+    website: "https://mmtaylor7.github.io/Doomsday_Tracker/",
   },
   {
     id: 2,
     image: lkjhl,
     projectTitle: "A Daily Dose of Code",
-    description: "An app that gives coders daily challenges to practice their skills",
-    github:
-      "https://github.com/mmtaylor7/daily-dose-of-code",
-    website: "https://desolate-escarpment-26537.herokuapp.com/"
+    description:
+      "An app that gives coders daily challenges to practice their skills",
+    github: "https://github.com/mmtaylor7/daily-dose-of-code",
+    website: "https://desolate-escarpment-26537.herokuapp.com/",
   },
   {
     id: 3,
     image: lkjhl,
     projectTitle: "Weather App",
-    description: "An app that allows users to view the weather outlook for several cities",
-    github:
-      "https://github.com/mmtaylor7/Weather-Dashboard",
-    website: "https://mmtaylor7.github.io/Weather-Dashboard/"
-  }
-  
+    description:
+      "An app that allows users to view the weather outlook for several cities",
+    github: "https://github.com/mmtaylor7/Weather-Dashboard",
+    website: "https://mmtaylor7.github.io/Weather-Dashboard/",
+  },
 ];
