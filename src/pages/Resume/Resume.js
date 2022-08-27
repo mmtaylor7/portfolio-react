e3a3d0e0a4e7d17cc40aa5f666e1c32615147bc2
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-26 22:57:19 -0400

    resume page

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -2,5 +2,12 @@ import { Routes, Route, Link } from "react-router-dom";
 import "../Resume/Resume.css";
 
 export default function Resume() {
-  return;
+  return (
+    <div>
+      <div className="page-title">Resume</div>
+      <div className="under-construction">
+        This page is under construction. Thank you for your patience!
+      </div>
+    </div>
+  );
 }
