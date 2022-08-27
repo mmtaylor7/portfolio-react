fef51f72476ea72dd0e3384fa21c87877710cada
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-26 22:38:07 -0400

    added footer links and mail to

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,12 +2,34 @@ import "../Footer/Footer.css";
 import React from "react";
 
 export default function Footer() {
+  const externalLink = () => {
+    window.location.replace(destination);
+  };
+
   return (
     <div className="footer-center">
       <footer className="footer-style text-center text-lg-start">
-        <div className="text-center p-3 footer-text">- LinkedIn -</div>
-        <div className="text-center p-3 footer-text">- GitHub -</div>
-        <div className="text-center p-3 footer-text">- Email -</div>
+        <div
+          className="text-center p-3 footer-text"
+          onClick={window.open(
+            "mailto:email@example.com?subject=Subject&body=Body%20goes%20here"
+          )}
+        >
+          - LinkedIn -
+        </div>
+
+        <div
+          className="text-center p-3 footer-text"
+          onClick={externalLink("https://github.com/mmtaylor7")}
+        >
+          - GitHub -
+        </div>
+        <div
+          className="text-center p-3 footer-text"
+          onClick={externalLink("taylor.marymargaret@gmail.com")}
+        >
+          - Email -
+        </div>
       </footer>
     </div>
   );
