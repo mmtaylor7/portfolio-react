876602187a4b145417bb35bb9cf5adfb61356059
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-26 22:50:06 -0400

    made onclicks take in functions

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import "../Footer/Footer.css";
 import React from "react";
 
 export default function Footer() {
-  const externalLink = () => {
+  const externalLink = (destination) => {
     window.location.replace(destination);
   };
 
@@ -11,22 +11,26 @@ export default function Footer() {
       <footer className="footer-style text-center text-lg-start">
         <div
           className="text-center p-3 footer-text"
-          onClick={window.open(
-            "mailto:email@example.com?subject=Subject&body=Body%20goes%20here"
-          )}
+          onClick={() =>
+            externalLink("www.linkedin.com/in/mary-margaret-taylor-12985117b")
+          }
         >
           - LinkedIn -
         </div>
 
         <div
           className="text-center p-3 footer-text"
-          onClick={externalLink("https://github.com/mmtaylor7")}
+          onClick={() => externalLink("https://github.com/mmtaylor7")}
         >
           - GitHub -
         </div>
         <div
           className="text-center p-3 footer-text"
-          onClick={externalLink("taylor.marymargaret@gmail.com")}
+          onClick={() =>
+            window.open(
+              "mailto:taylor.marymargaret@gmail.com?subject=Subject&body=Body%20goes%20here"
+            )
+          }
         >
           - Email -
         </div>
